066a679b6add977a281a942670bc431baa976964
Michael Schoebel <mschoebel@mschoebel.de>
2015-04-22 15:46:06 +0200

    Simplify Linux build-script

--- build-linux.sh
+++ build-linux.sh
@@ -1,48 +1,44 @@
 #!/bin/bash
 echo "Starting Linux build of OpenAcoon..."
 echo "------------------------------------"
-rm src/*.o
-rm src/*.ppu
-rm src/cgi/*.o
-rm src/cgi/*.ppu
 mkdir bin
-rm bin/*
+mkdir bin/units
+rm bin/units/*.o
+rm bin/units/*.ppu
+rm bin/units/*.rst
 set -e
 
 compiler="fpc -Mdelphi -Tlinux -O3 -vew -vq"
-compiler=$compiler" -Fudep/indy10/Lib/*"
+compiler=$compiler" -Fusrc/dep/indy10/Lib/*"
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
 # You will probably need to change the path in the following line to match your setup
 #
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 compiler=$compiler" -Fu/usr/lib64/fpc/2.6.4/units/x86_64-linux/*"
-compiler=$compiler" -FE."
+compiler=$compiler" -FEbin -FUbin/units"
 
 
 function compile {
     $compiler $1.dpr
-    if [ -e "$1" ]
-    then
-        mv $1 ../bin
-    fi
     echo -e '\n'
 }
 
 
-cd src
+#cd src
 
-compile RobotNew
-compile searchservernew
-compile ImportUrls
-compile CleanUrlsTxt
-compile PrepareRobot
-compile ImportData
-compile Sleep
-compile Parser
-compile GenDb
-compile cgi/query
-mv query ../bin
+compile src/RobotNew
+compile src/searchservernew
+compile src/ImportUrls
+compile src/CleanUrlsTxt
+compile src/PrepareRobot
+compile src/ImportData
+compile src/Sleep
+compile src/Parser
+compile src/GenDb
+compile src/cgi/query
+
+# The following two only compile with Delphi. Needs fixing.
 #compile Robot
 #compile SearchServer
 
