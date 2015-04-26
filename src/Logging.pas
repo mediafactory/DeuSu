9c8ccb8ff06f5a23a2cc99f75e02ef141ac24a3d
Michael Schoebel <mschoebel@mschoebel.de>
2015-04-26 20:07:16 +0200

    Code cleanup
    - Reformatting according to style-guide

--- src/Logging.pas
+++ src/Logging.pas
@@ -1,60 +1,68 @@
 unit Logging;
 
 (*
-    DeuSu - An OpenSource Internet-Search-Engine
-    Copyright (C) 1999-2015 Michael Schoebel & Acoon GmbH
+  DeuSu - An OpenSource Internet-Search-Engine
+  Copyright (C) 1999-2015 Michael Schoebel & Acoon GmbH
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License version 2 as
-    published by the Free Software Foundation.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2 as
+  published by the Free Software Foundation.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 *)
 
 interface
 
-procedure LogMsg(const aFilename, aMessage: string);
-procedure DebugLogMsg(const aFilename, aMessage: string);
+procedure LogMsg(const AFilename, AMessage: string);
+procedure DebugLogMsg(const AFilename, AMessage: string);
+
 
 
 implementation
 
 uses
-    SysUtils,
-    SyncObjs;
+  SysUtils,
+  SyncObjs;
 
 var
-    CritSec: TCriticalSection;
+  CritSec: TCriticalSection;
 
 
-procedure DebugLogMsg(const aFilename, aMessage: string);
+
+procedure DebugLogMsg(const AFilename, AMessage: string);
 begin
     // LogMsg(aFilename, aMessage);
 end;
 
 
 
-procedure LogMsg(const aFilename, aMessage: string);
+procedure LogMsg(const AFilename, AMessage: string);
 var
-    f: TextFile;
+  f: TextFile;
 begin
-    CritSec.Enter;
-    try
-        AssignFile(f, aFilename);
-        if FileExists(aFilename) then Append(f)
-        else ReWrite(f);
-        WriteLn(f, FormatDateTime('yyyy-mm-dd hh:nn:ss', Now), ' ', aMessage);
-        CloseFile(f);
-    except
-    end;
-    CritSec.Leave;
+  CritSec.Enter;
+
+  try
+    AssignFile(f, aFilename);
+    if FileExists(aFilename) then
+      Append(f)
+    else
+      ReWrite(f);
+
+    WriteLn(f, FormatDateTime('yyyy-mm-dd hh:nn:ss', Now), ' ',
+      aMessage);
+
+    CloseFile(f);
+  except
+  end;
+
+  CritSec.Leave;
 end;
 
 
 begin
-    CritSec := TCriticalSection.Create;
-
+  CritSec := TCriticalSection.Create;
 end.
