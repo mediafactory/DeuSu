e655edeba8d4662b4e38cc6e8a5ef01ed1648da2
Michael Schoebel <mschoebel@mschoebel.de>
2016-01-29 11:24:01 +0100

    Update version number and date

--- src/GlobalTypes.pas
+++ src/GlobalTypes.pas
@@ -25,11 +25,11 @@ const
   cPathDelimiter = '\';
 {$ENDIF}
 
-  cShortVersion = '5.1.0';
-  cDate = '30-Nov-2015';
+  cShortVersion = '5.1.1';
+  cDate = '29-Jan-2016';
   cVersion = cShortVersion+'   '+cDate;
 
-  cCopyright = '(c) 1999-2015 Michael Schoebel & Acoon GmbH';
+  cCopyright = '(c) 1999-2016 Michael Schoebel & Acoon GmbH';
   cVersionCopy = cVersion+cCRLF+cCopyright;
   cGPLNotice =
     'This program is licensed under GPL version 2 (see LICENSE.TXT)'+
