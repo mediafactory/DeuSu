c0f1c0d7c9bb8628a90cf0d9264f593a8516afab
Henner <hg@mediafactory.de>
2016-10-13 16:49:21 +0200

    + vagrant

---
+++ vagrant/linux/bootstrap.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+VERSION=$(sed 's/\..*//' /etc/debian_version)
+
+# debian packages
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -y install software-properties-common
+
+# FPC 3.0 is in debian SID
+add-apt-repository -y "deb http://ftp.de.debian.org/debian/ unstable main contrib non-free"
+add-apt-repository -y "deb-src ttp://ftp.de.debian.org/debian/  unstable main contrib non-free"
+# deb     http://security.debian.org/         unstable/updates  main contrib non-free
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install -y fpc apache2 lynx
+
+ln -s /vagrant_project/ /home/vagrant/DeuSu 2>/dev/null
+
+cd /home/vagrant/DeuSu
+./build-linux.sh
+
+# create an empty URL-database
+cd bin
+./ImportUrls
+
+cp ../domainrank.txt data/txt/urls.txt
